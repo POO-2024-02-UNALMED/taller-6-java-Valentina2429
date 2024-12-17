e9651e215dfeec0bac894e5bc38caea9821a8381
Valentina Leon Beltran <vleonb@unal.edu.co>
2024-12-16 22:43:35 -0500

    Update Fabricante.java

--- src/main/java/Fabricante.java
+++ src/main/java/Fabricante.java
@@ -10,10 +10,10 @@ public class Fabricante {
     }
 
     public String getNombre() { return nombre; }
-    public void setNombre(String nombre){this.nombre=nombre }
+    public void setNombre(String nombre){this.nombre=nombre; }
 
     public Pais getPais() { return pais; }
-    public void setPais(Pais pais){this.pais=pais }
+    public void setPais(Pais pais){this.pais=pais; }
 
     public static Fabricante fabricaMayorVentas() {
         int maxVentas = 0;
